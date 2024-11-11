8ca6046ed17d82b760ee74912b862f81378d6bd5
toddy <164919178+lorenasoares16@users.noreply.github.com>
2024-11-11 14:14:42 -0300

    coisa

---
+++ img/perguntas.js
@@ -0,0 +1,11 @@
+criarCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação.'
+) 
+
+criarCartao(
+    'geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris .'
+)
